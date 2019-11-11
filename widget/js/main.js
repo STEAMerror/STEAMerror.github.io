a5efe1a0a2a601f031941a7d9a5ed70345876790
STEAMerror <55356151+STEAMerror@users.noreply.github.com>
2019-11-12 01:20:08 +0300

    Add files via upload

--- widget/js/main.js
+++ widget/js/main.js
@@ -1,11 +1,44 @@
 
 $(document).ready(function(){
    $.ajax({
-        url:'php/widget.php',
+        url:'php/widget_api.php',
 		cache: false,
-       success:function(data)
+       success:function()
        {
-              $(".animationWidget").html(data);
-           }
+        setTimeout(wid1, 0);
+        function wid1 (){
+        $.ajax({
+            url:'php/widget1.php',
+            cache: false,
+           success:function(data)
+           {
+            $(".animationWidget").html('');
+            $(".animationWidget").html(data); 
+            setTimeout(wid2, 3700);  
+              
+               }
+        });
+    }
+    
+    
+        function wid2 (){
+        $.ajax({
+            url:'php/widget2.php',
+            cache: false,
+           success:function(data)
+           {
+            $(".animationWidget").html('');
+            $(".animationWidget").html(data);
+            setTimeout(wid1, 3700);
+        
+               }
+        });
+    }
+        
+           
+    }
+           
     });
+
+    
 });
---
+++ widget/php/widget2.php
@@ -0,0 +1,34 @@
+<?php
+        
+require_once 'connect.php';
+$result = mysqli_query ($con, "SELECT * from `weatherparam` WHERE `id_city`=2");
+while($name = mysqli_fetch_assoc($result)) {
+    $cityName = $name['city_name'];
+    $temp_c = $name['temp_c'];
+    $humidity = $name['humidity'];
+    $wind = $name['wind'];
+    $pressure = $name['pressure'];
+    $weatherArr = $name['icon_id'];
+}
+mysqli_close($con);
+?>
+<span class="nameCity"><?php 
+require_once 'trans_func.php';
+    print transliterateen($cityName);?></span>
+    <hr>
+
+    <div class="weatherAndTemp">   
+    <span class="temp"> <?php if ($temp_c >= 0) {print '+';} else {print '-';}; print $temp_c; ?> C&deg; 
+ 
+    </span> 
+    <?php  
+    echo '<img class="imgWeather" src="https://openweathermap.org/img/wn/' .
+    $weatherArr . '@2x.png">'; 
+    ?></div>
+
+    <img class="icon" src="img/Humidity.png">  <span class="param"><?php print $humidity; ?>% </span><br>
+    <img class="icon" src="img/wind.png"> <span class="param"><?php print $wind; ?> м/c </span><br>
+    <img class="icon" src="img/pressure.png"> <span class="param"> 
+    <?php 
+        print round($pressure, 0);
+    ?> мм рт. ст. </span>
